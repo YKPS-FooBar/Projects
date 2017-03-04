b8094b68a604c05406d4af348d86d2a45a78cee5
XS150 <2928293360@qq.com>
2017-03-04 08:06:27 +0800

    Update kevin_wang_2.py

--- 002 - Guess the Number Advanced/kevin_wang_2.py
+++ 002 - Guess the Number Advanced/kevin_wang_2.py
@@ -4,7 +4,7 @@ height_number=1000 #enter the greatest number that you want others to guess
 A=random.randint(lowest_number,height_number)
 times=5 #enter the time you want other to guess
 print("welcome to the 'guess the number'game!!!\n \nIn this game, you should guess a whole number from",lowest_number,"to",height_number," \n \n  ")
-A_1=int(input("please enter a guess(only a number):"))
+A_1=int(input("please enter a guess(numbers only):"))
 for chance in range(times):
         if A_1 == A:
             print("well done!!!\n \n \nYou get the answer!")
