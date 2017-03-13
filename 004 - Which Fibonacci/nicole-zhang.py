19ffe9bb93af7e6e8737d84b2fb8c93285aa49d9
NMRZ <nicolecole0810@hotmail.com>
2017-03-13 15:57:31 +0800

    Create nicole-zhang.py

---
+++ 004 - Which Fibonacci/nicole-zhang.py
@@ -0,0 +1,11 @@
+fibo = [1,1]
+n = int(input("."))
+if n == 1:
+    print("1 or 2")
+else:
+    while n > fibo[-1]:
+        fibo.append(fibo[-1]+fibo[-2])
+    if n == fibo[-1]:
+        print(fibo.index(n)+1)
+    else:
+        print(".")
