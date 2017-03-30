06e30a73f0fb96893dbd6fbea7b4a07f949637d7
XS150 <2928293360@qq.com>
2017-03-30 13:27:29 +0800

    Update kevin-wang.py

--- 005 - Magic Backpack/kevin-wang.py
+++ 005 - Magic Backpack/kevin-wang.py
@@ -3,7 +3,7 @@ Backpack = b
 print("Hello， well come to the Backpack. Here you can add or delete things in the backpack in order to creat your own personal backpack. \
 \nThe things you can do：\
 \n1.Type:[add xxx (the nth place you want the item at)] to add an item into the backpack.\
-\n2.Type:[delete xxx (the nth place you want the item at)] to delete an item from the backpack.\
+\n2.Type:[delete xxx(name)] or [delete xxx(nth term)] to delete an item from the backpack.\
 \n3.Type:[Backpack] to show the things inside your backpack.\
 \n4.Type:[help] to show the things you can do and the comman for them.")
 while 1 == 1:
@@ -15,9 +15,19 @@ while 1 == 1:
         b.insert(d, a)
     if "delete" in i:
         e = i.split()[1]
-        f = int(i.split()[2])
-        g = f-1
-        b.insert(g, e)
+        if e in b:
+            b.revome(e)
+        else:
+            e = int(e)
+            if len(b) >= e-1:
+                b.remove(b[e-1])
+            else:
+                if len(b) == 1:
+                    print("\nThere is only one term in the backpack, you cannot delete the",e,"term.")
+                elif len(b) == 0:
+                    print("There is no term left in the backpack, you cannot delete the",e,"term.")
+                else:
+                    print("There is only",len(b),"term left in the backpack, you cannot delete the",e,"term.")
     if "Backpack" in i:
         print()
         print(*Backpack, sep=', ')
