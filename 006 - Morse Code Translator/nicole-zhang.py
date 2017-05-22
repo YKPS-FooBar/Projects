c357a0a10d705944a3ca571a8351e2269b01be5b
NMRZ <nicolecole0810@hotmail.com>
2017-05-22 15:59:29 +0800

    Update nicole-zhang.py

--- 006 - Morse Code Translator/nicole-zhang.py
+++ 006 - Morse Code Translator/nicole-zhang.py
@@ -1,3 +1,4 @@
+#decode
 morse = {'.-': 'a', '-...': 'b', '-.-.': 'c', '.': 'd'}
 word = input(':')
 result = ''
@@ -8,3 +9,15 @@ for i in word.split('   '):
     result = result + ' '
         
 print(result)
+
+#incode
+morse = {'a':'.-', 'b':'-...', 'c':'-.-.', 'd':'.'}
+word = input(': ')
+word = word.lower()
+length = len(word)
+n = 0
+x = ''
+while n < length:
+    x = x + morse[word[n]] + '  ' 
+    n = n + 1
+print(x)
